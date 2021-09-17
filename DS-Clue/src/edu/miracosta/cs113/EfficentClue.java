6ae2cfa1f2f2d28bc10b7adbcf5b5c1f2b1205f6
andrewcomputsci2019 <54915639+andrewcomputsci2019@users.noreply.github.com>
2021-09-16 21:20:57 -0700

    finished algo and test related to it

--- DS-Clue/src/edu/miracosta/cs113/EfficentClue.java
+++ DS-Clue/src/edu/miracosta/cs113/EfficentClue.java
@@ -1,4 +1,69 @@
 package edu.miracosta.cs113;
 
+import model.AssistantJack;
+import model.Theory;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class EfficentClue {
+
+    private int[] lookUp  = {1,1,1}; // weapon location person
+    private int guessCount =0;
+
+    public EfficentClue(){
+
+    }
+
+    /**
+     * Method to find the correct solution of the given answer set
+     * @param jack the needed helper for this method
+     * @return the correct theory
+     */
+    private Theory findSolution(AssistantJack jack){
+        while (lookUp[2]<=6 && lookUp[1]<=10 && lookUp[0]<=20){
+            int test = jack.checkAnswer(lookUp[0],lookUp[1],lookUp[2]);
+            if (test==0){
+                break;
+            }else{
+                lookUp[test-1]++;
+            }
+        }
+        return new Theory(lookUp[0],lookUp[1],lookUp[2]);
+    }
+
+    /**
+     *
+     * @param answerSet the answer set to be used
+     * @return a theory of the correct answer from the answer set
+     */
+    public Theory getSolution(int answerSet){
+        AssistantJack jack = new AssistantJack(answerSet);
+        Theory temp = findSolution(jack);
+        guessCount = jack.getTimesAsked();
+        return temp;
+    }
+    public Theory getSolution(AssistantJack jack){
+        Theory temp = findSolution(jack);
+        guessCount = jack.getTimesAsked();
+        return temp;
+    }
+    public int getGuessCount(){
+        return guessCount;
+    }
+
+    public static void main(String[] args) {
+        EfficentClue efficentClue = new EfficentClue();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("enter 1 for set 1, 2 for set 2, 3 for random");
+        int set  = scanner.nextInt();
+        Theory theory = efficentClue.getSolution(set);
+        if (efficentClue.getGuessCount()<=20){
+            System.out.println("took less than 20 times");
+            System.out.println(theory);
+        }else{
+            System.out.println("failed to find correct answer in 20 guess");
+        }
+    }
+
 }
---
+++ DS-Clue/test/TheoryTest.java
@@ -0,0 +1,64 @@
+import edu.miracosta.cs113.EfficentClue;
+import model.AssistantJack;
+import static org.junit.Assert.*;
+
+import model.Theory;
+import org.junit.Test;
+
+import java.lang.reflect.Field;
+
+public class TheoryTest {
+    private final EfficentClue clue = new EfficentClue();
+    @Test
+    public void testSet1(){
+        AssistantJack jack = new AssistantJack(1);
+        Field theory;
+        Theory correct = null;
+        try { // used to get the correct theory without causing an increase in guess count
+            theory = jack.getClass().getDeclaredField("correctTheory");
+            theory.setAccessible(true);
+            correct = (Theory) theory.get(jack);
+        }catch (NoSuchFieldException | IllegalAccessException e){
+            e.printStackTrace();
+            fail("failed to get theory");
+        }
+        assertTrue(isEqual(correct,clue.getSolution(jack)));
+        assertTrue("not less than 20",jack.getTimesAsked()<=20);
+    }
+    @Test
+    public void testSet2(){
+        AssistantJack jack = new AssistantJack(2);
+        Field theory;
+        Theory correct = null;
+        try { // used to get the correct theory without causing an increase in guess count
+            theory = jack.getClass().getDeclaredField("correctTheory");
+            theory.setAccessible(true);
+            correct = (Theory) theory.get(jack);
+        }catch (NoSuchFieldException | IllegalAccessException e){
+            e.printStackTrace();
+            fail("failed to get theory");
+        }
+        assertTrue(isEqual(correct,clue.getSolution(jack)));
+        assertTrue("not less than 20",jack.getTimesAsked()<=20);
+    }
+    @Test
+    public void testRandom()  {
+        AssistantJack jack = new AssistantJack(3);
+        Field theory;
+        Theory correct = null;
+        try { // used to get the correct theory without causing an increase in guess count
+            theory = jack.getClass().getDeclaredField("correctTheory");
+            theory.setAccessible(true);
+            correct = (Theory) theory.get(jack);
+        }catch (NoSuchFieldException | IllegalAccessException e){
+            e.printStackTrace();
+            fail("failed to get theory");
+        }
+        assertTrue(isEqual(correct,clue.getSolution(jack)));
+        assertTrue("not less than 20",jack.getTimesAsked()<=20);
+    }
+
+    private boolean isEqual(Theory first, Theory other){
+        return first.getLocation()==other.getLocation() && first.getWeapon() == other.getWeapon() && first.getPerson() == other.getPerson();
+    }
+}
