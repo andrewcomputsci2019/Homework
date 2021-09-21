84c167d28009790c9a1d03a12e280ee2299d31ee
andrewcomputsci2019 <54915639+andrewcomputsci2019@users.noreply.github.com>
2021-09-21 13:56:18 -0700

    most of poloynomial has been added and mostly works

--- DS-Polynomial/src/code/Polynomial.java
+++ DS-Polynomial/src/code/Polynomial.java
@@ -1,7 +1,51 @@
 package code;
 
-public class Polynomial {
-    public Polynomial(){
+import java.util.LinkedList;
 
+public class Polynomial {
+
+    private LinkedList<Term> terms;
+
+    public Polynomial(){
+        terms = new LinkedList<>();
+    }
+    public Polynomial(Polynomial original){
+        terms = new LinkedList<>();
+        for(Term term:original.terms){
+            terms.add(term.clone());
+        }
+    }
+
+    //@TODO see comment at bottom
+    public void addTerm(Term term){
+        terms.add(term);
+    }
+    public int getNumTerms(){
+        return terms.size();
+    }
+    public Term getTerm(int index){
+        return terms.get(index);
+    }
+    public void clear(){
+        terms.clear();
+    }
+    //@TODO see comment at bottom
+    public Polynomial add(Polynomial other){
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        if (terms==null||terms.size()==0){
+            return "0";
+        }
+        int counter = 0;
+        for (Term term: terms){
+            builder.append(counter++==0?term.toString().substring(1):term.toString());
+        }
+        return builder.toString().replaceAll("[|]","");
     }
 }
+// @TODO terms sorted when enter and combine like terms when entered
+// @TODO make the add method so two polynomials can be added together
