c167d184b117f7cac40a7bb0719ad02bfc7fd0f7
andrewcomputsci2019 <54915639+andrewcomputsci2019@users.noreply.github.com>
2021-10-06 16:33:40 -0700

    finished arraylist stack and passed all test

--- DS-StackQueue/src/edu/miracosta/cs113/ArrayListStack.java
+++ DS-StackQueue/src/edu/miracosta/cs113/ArrayListStack.java
@@ -1,2 +1,40 @@
-package edu.miracosta.cs113;public class ArrayListStack {
+package edu.miracosta.cs113;
+
+import java.util.ArrayList;
+import java.util.EmptyStackException;
+
+public class ArrayListStack<E> implements StackInterface<E>{
+    private final ArrayList<E> queue;
+    public ArrayListStack(){
+        queue = new ArrayList<>();
+    }
+
+
+    @Override
+    public boolean empty() {
+        return queue.size()==0;
+    }
+
+    @Override
+    public E peek() {
+        checkSize();
+        return queue.get(queue.size()-1);
+    }
+
+    @Override
+    public E pop() {
+        checkSize();
+        return queue.remove(queue.size()-1);
+    }
+
+    @Override
+    public E push(E obj) {
+        queue.add(obj);
+        return obj;
+    }
+    private void checkSize(){
+        if (empty()){
+            throw new EmptyStackException();
+        }
+    }
 }
